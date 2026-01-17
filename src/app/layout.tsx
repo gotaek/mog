cec48867a78da6482c62f137cd9180694917aec1
gotaek <deriko@naver.com>
2026-01-17 15:05:55 +0900

    feat: Add Microsoft Clarity integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,15 @@ export default function RootLayout({
             gtag('config', 'G-Z4Z4LPG6KH');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v2pz0g1lhi");
+          `}
+        </Script>
       </body>
     </html>
   );
